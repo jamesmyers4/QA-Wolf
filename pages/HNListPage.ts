6d05504b08d8a90ba20d6e81f3c2b690c5336b9b
jamesmyers4 <james.myers4@gmail.com>
2026-07-23 07:25:45 -0400

    Trim dead POM surface (StoryRow.ts and HNListPage.ts)

--- pages/HNListPage.ts
+++ pages/HNListPage.ts
@@ -3,16 +3,12 @@ import { StoryRow } from "./StoryRow";
 import { settleRateLimit } from "../helpers/settleRateLimit";
 
 export class HNListPage {
-  readonly navLinks: Locator;
-  readonly loginLink: Locator;
   readonly moreLink: Locator;
 
   constructor(
     private readonly page: Page,
     readonly url: string,
   ) {
-    this.navLinks = page.locator("#hnmain .pagetop a");
-    this.loginLink = page.locator('a[href^="login"]');
     this.moreLink = page.locator("a.morelink");
   }
 
@@ -34,15 +30,7 @@ export class HNListPage {
     return this.page.locator("tr.athing").count();
   }
 
-  getStoryByRank(rank: number): StoryRow {
-    return new StoryRow(this.page.locator("tr.athing").nth(rank - 1));
-  }
-
   async clickMore(): Promise<void> {
     await this.moreLink.click();
-  }
-
-  async isLoggedIn(): Promise<boolean> {
-    return !(await this.loginLink.isVisible());
   }
 }
